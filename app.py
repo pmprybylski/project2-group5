cc23432a974f6f7fa71a0ec87fb0b5eb3983c0c4
HopeNeely <hope.neely@gmail.com>
2021-04-16 08:20:32 -0400

    added 4th route for 4th table and added object lables.

--- app.py
+++ app.py
@@ -21,63 +21,18 @@ conn = psycopg2.connect("dbname=squirrel_census user={user} password={password}"
 cursor = conn.cursor()
 
 # Grab each table and put into list
-select_tables = ["select * from squirrel_interact", "select * from squirrel_loc", "select * from squirrel_map_loc", "select * from squirrel_mod"]
+select_tables = [
+    "select * from squirrel_interact", 
+    "select * from squirrel_loc", 
+    "select * from squirrel_map_loc", 
+    "select * from squirrel_mod"
+    ]
 
 squirrel_tables = [] 
 for select_table in select_tables:
     cursor.execute(select_table)
     table = cursor.fetchall()
     squirrel_tables.append(table)
-
-"""
-squirrel_tables cheat sheet: 
-------------------------------
-squirrel_tables[0] = squirrel_interact
-row[0] = lat
-row[1] = lng
-row[2] = unique_squirrel_id
-row[3] = date
-row[4] = approaches
-row[5] = indifferent
-row[6] = runs_from
-row[7] = lat_lng
-
-squirrel_tables[1] = squirrel_loc
-row[0] = lat
-row[1] = lng
-row[2] = unique_squirrel_id
-row[3] = date
-row[4] = lat_lng
-
-squirrel_tables[2] = squirrel_map_loc
-row[0] = lat
-row[1] = lng
-row[2] = unique_squirrel_id
-row[3] = date
-row[4] = hectare
-row[5] = hectare_squirrel_no
-row[6] = lat_lng
-
-squirrel_tables[3] = squirrel_squirrel_mod
-row[0] = lat
-row[1] = lng
-row[2] = unique_squirrel_id
-row[3] = hectare
-row[4] = shift
-row[5] = date
-row[6] = hectare_squirrel_no
-row[7] = age 
-row[8] = primary_fur_color
-row[9] = highlight_fur_color
-row[10] = combination_colors
-row[11] = color_notes
-row[12] = approaches
-row[13] = indifferent
-row[14] = runs_from
-row[15] = other_interactions
-row[16] = lat_lng
-
-"""
 
 
 """
@@ -94,51 +49,113 @@ def home():
 
     return(
         f'Flask Routes:<br/>'
+        f'/squirrels_location<br/>'
         f'/map_squirrels_location<br/>' 
         f'/map_friendly_squirrels<br/>'
         f'/plot_squirrel_colors'
         )
 
 
-@app.route("/map_squirrels_location")
+@app.route("/squirrels_location")
 def location():
     
     location = squirrel_tables[1]
-    
-    map_location = squirrel_tables[2]
 
+    location_list = []
+    for row in location:
+        dict3 = {}
+        dict3["lat"] = row[0]
+        dict3["lng"] = row[1] 
+        dict3["unique_squirrel_id"] = row[2] 
+        dict3["date"] = row[3]
+        dict3["lat_lng"] = row[4]
+        location_list.append(dict3)
     
     """
     Return the JSON representation of data.
     """
     print("Server recieved request...")
-    return jsonify(location)
-    
+    return jsonify(location_list)
 
+@app.route("/map_squirrels_location")
+def map_location():
+    
+    map_location = squirrel_tables[2]
+
+    map_location_list = []
+    for row in map_location:
+        dict4 = {}
+        dict4["lat"] = row[0]
+        dict4["lng"] = row[1] 
+        dict4["unique_squirrel_id"] = row[2] 
+        dict4["date"] = row[3]
+        dict4["hectare"] = row[4]
+        dict4["hectare_squirrel_no"] = row[5]
+        dict4["lat_lng"] = row[6]
+        map_location_list.append(dict4)
+
+    """
+    Return the JSON representation of data.
+    """
+    print("Server recieved request...")
+    return jsonify(map_location_list)
 
 @app.route("/map_friendly_squirrels")
 def friendly():
 
     friendly = squirrel_tables[0]
 
+    friendly_list = []
+    for row in friendly:
+        dict2 = {}
+        dict2["lat"] = row[0]
+        dict2["lng"] = row[1] 
+        dict2["unique_squirrel_id"] = row[2] 
+        dict2["date"] = row[3]
+        dict2["approaches"] = row[4]
+        dict2["indifferent"] = row[5]
+        dict2["runs_from"] = row[6]
+        dict2["lat_lng"] = row[7]
+        friendly_list.append(dict2)
 
     """
     Return the JSON representation of data.
     """
     print("Server recieved request...")
-    return jsonify(friendly)
-
+    return jsonify(friendly_list)
 
 @app.route("/plot_squirrel_colors")
 def colors():
 
     squirrel_color = squirrel_tables[3]
 
+    demo_list = []
+    for row in squirrel_color:
+        dict1 = {}
+        dict1["lat"] = row[0]
+        dict1["lng"] = row[1]
+        dict1["unique_squirrel_id"] = row[2] 
+        dict1["hectare"] = row[3]
+        dict1["shift"] = row[4]
+        dict1["date"] = row[5]
+        dict1["hectare_squirrel_no"] = row[6]
+        dict1["age"] = row[7]
+        dict1["primary_fur_color"] = row[8]        
+        dict1["highlight_fur_color"] = row[9] 
+        dict1["combination_colors"] = row[10]
+        dict1["color_notes"] = row[11]
+        dict1["approaches"] = row[12]
+        dict1["indifferent"] = row[13]
+        dict1["runs_from"] = row[14]        
+        dict1["other_interactions"] = row[15]
+        dict1["lat_lng"] = row[16]
+        demo_list.append(dict1)
+
     """
     Return the JSON representation of data.
     """
     print("Server recieved request...")
-    return jsonify(squirrel_color)
+    return jsonify(demo_list)
 
 
 if __name__ == "__main__":
